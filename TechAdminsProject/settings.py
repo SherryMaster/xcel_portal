8fa4b9f0c7f1ac7e4eb8dbd5f3948a9c16aa6c7f
SherryMaster <128972759+SherryMaster@users.noreply.github.com>
2023-12-14 12:17:40 +0500

    Update settings.py
    Allowed host to all

--- TechAdminsProject/settings.py
+++ TechAdminsProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(7&ank)!*buqh70#v@-x8p-o#k=(umxg40ow+n=^e#yvpq$=cy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
