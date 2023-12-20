ad93422fbb728bbb09c8436c2eefd9e6d9beb024
SherryMaster <128972759+SherryMaster@users.noreply.github.com>
2023-12-20 19:50:09 +0500

    Made some mess up with user and profile model

--- HR_App/models.py
+++ HR_App/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from datetime import datetime
 
 from django.utils import timezone
 
-
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_year = models.CharField(default=datetime.now().year, max_length=4)
     profile_month = models.CharField(default=datetime.now().month, max_length=2)
+    ##
+    profile_minute = models.CharField(default=datetime.now().minute, max_length=2)
+    ##
     login_time = models.CharField(null=True, blank=True, max_length=200)
     logout_time = models.CharField(null=True, blank=True, max_length=200)
     break_start_time = models.CharField(null=True, blank=True, max_length=200)
@@ -22,9 +23,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username + ' ' + self.profile_year + '-' + self.profile_month
 
+class CustomUser(AbstractUser):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 class Logs(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     time = models.TimeField(auto_now_add=True)
     date = models.DateField(auto_now_add=True)
     message = models.CharField(max_length=200)
--- TechAdminsProject/settings.py
+++ TechAdminsProject/settings.py
@@ -139,3 +139,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+AUTH_USER_MODEL = 'HR_App.CustomUser'
