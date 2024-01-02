3173cb6edbd6b020ebb774aaf967344f8956f22d
SherryMaster <128972759+SherryMaster@users.noreply.github.com>
2024-01-02 15:50:41 +0500

    Modified date field

--- HR_App/models.py
+++ HR_App/models.py
@@ -26,7 +26,7 @@ class Logs(models.Model):
 
 class Performance(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(null=True, blank=True, default=datetime.datetime.now().date())
+    date = models.DateField(auto_now_add=True)
     login_time = models.TimeField(null=True, blank=True, default=datetime.datetime.now().time())
     work_time = models.IntegerField(null=True, blank=True, default=0)
     break_time = models.IntegerField(null=True, blank=True, default=0)
